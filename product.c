b297b36f07910e99bae65e26aa68ac9e65d58ae0
haram22 <kimhr9889@handong.ac.kr>
2022-04-18 00:04:48 +0900

    nine

--- product.c
+++ product.c
@@ -6,5 +6,9 @@ void menu(){
     printf("2. 추가\n");
     printf("3. 수정\n");
     printf("4. 삭제\n");
-    printf("5. 종료\n\n");
+    printf("5. 상품명 검색\n");
+    printf("6. 주소 검색\n");
+    printf("7. 상품 가격 검색\n");
+    printf("8. 저장\n");
+    printf("9. 종료\n\n");
 }
